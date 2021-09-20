9d6e9cdb15efa4199bc90fdb61f644a5b8867a92
Alex Parker <alex.n.parker82@gmail.com>
2021-09-19 21:22:42 -0400

    working on api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,26 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const categories = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categories);
+  } catch {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category =  await Category.findByPk()
+
+  }
 });
 
 router.post('/', (req, res) => {
